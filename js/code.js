af347ef5c203449e64a2590ec2f869fcd5a5ca08
Alex Gallichotte <agallichotte@nationalresearch.com>
2013-09-06 19:59:35 -1000

    Populate list of videos via jquery

--- js/code.js
+++ js/code.js
@@ -1,17 +1,59 @@
+/* knockout models */
+
 function Video(videoID) {
     this.videoID = videoID;
 
-    this.getThumbnailUrl = function () {
-        return "http://img.youtube.com/vi/" + this.videoID + "/default.jpg"
+    this.getThumbnailUrl = function() {
+        return "http://img.youtube.com/vi/" + this.videoID + "/default.jpg";
     }
-};
+}
 
 function VideosViewModel() {
-    self.videos = ko.observableArray([
-        new Video("rvdYly4A5W0"),
-        new Video("iaAkWy55V3A"),
-        new Video("1ZxN9iQM7OY")
-    ])
-};
+    var self = this;
 
-ko.applyBindings(new VideosViewModel());
+    self.videos = ko.observableArray();
+
+    self.loadVideos = function() {
+        $.ajax(
+            "/api/get_videos",
+            {
+                method: "post",
+                dataType: "json",
+                success: function(data) {
+                    var newVideos = $.map(data, function(i) {
+                        return new Video(i);
+                    });
+                    self.videos(newVideos);
+                }
+            });
+    }
+
+    self.loadVideos();
+}
+
+/* private methods */
+
+// configure ajax requests to pass csrf cookie
+function setupCsrf() {
+    var csrftoken = $.cookie('csrftoken');
+
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+
+    $.ajaxSetup({
+        crossDomain: false,
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type)) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+    }}});
+}
+
+/* code */
+
+setupCsrf();
+
+var model = new VideosViewModel();
+ko.applyBindings(model);
+model.loadVideos();
--- player/views.py
+++ player/views.py
@@ -1,9 +1,10 @@
 from django.template.loader import get_template
-from django.template import Context
+from django.template import RequestContext
 from django.http import HttpResponse
 
-def index(self):
+def index(request):
+    c = RequestContext(request)
+
     t = get_template('index.html')
-    c = Context({})
     html = t.render(c)
     return HttpResponse(html)

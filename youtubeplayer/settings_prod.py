b0b29ea874e3e57fb5c52185215d8f6a0e79c858
Alex Gallichotte <agallichotte@nationalresearch.com>
2013-09-09 20:20:49 -1000

    add BASE_DIR and STATIC_ROOT for heroku plz

--- youtubeplayer/settings_prod.py
+++ youtubeplayer/settings_prod.py
@@ -169,3 +169,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'

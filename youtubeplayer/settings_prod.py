dd38e78f5896e09158e901443711aba640037519
Alex Gallichotte <agallichotte@nationalresearch.com>
2013-09-09 20:24:45 -1000

    Add staticfiles_dirs heroku-stylee

--- youtubeplayer/settings_prod.py
+++ youtubeplayer/settings_prod.py
@@ -172,3 +172,9 @@ ALLOWED_HOSTS = ['*']
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'js'),
+    os.path.join(BASE_DIR, 'css'),
+)

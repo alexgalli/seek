80b23decf35d7a3b5e3f51f8e5f7a45afb287332
Alex Gallichotte <agallichotte@nationalresearch.com>
2013-10-26 17:23:45 -1000

    Add initial migration point

--- youtubeplayer/settings.py
+++ youtubeplayer/settings.py
@@ -127,6 +127,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
     'player',
     'account',
+    'south',
 )
 
 # A sample logging configuration. The only tangible logging
--- youtubeplayer/settings_prod.py
+++ youtubeplayer/settings_prod.py
@@ -128,6 +128,7 @@ INSTALLED_APPS = (
     'account',
     # for Heroku
     'gunicorn',
+    'south',
 )
 
 # A sample logging configuration. The only tangible logging
